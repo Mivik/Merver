d306a3d6939dfcca01bec757e25808573bda6985
Mivik <mivik@qq.com>
2019-12-01 17:08:15 +0800

    refined SimpleMerver

--- src/main/java/com/mivik/merver/SimpleMerver.java
+++ src/main/java/com/mivik/merver/SimpleMerver.java
@@ -3,9 +3,10 @@ package com.mivik.merver;
 public class SimpleMerver extends Merver {
 	@Override
 	public void process(Request req, Response res) {
+		System.out.println("=======REQUEST BEGIN=======");
 		System.out.println(req);
-		System.out.println("===================");
+		System.out.println("========REQUEST END========");
 		res.setResponseCode(200);
-		res.setData("Nothing");
+		res.setData("");
 	}
 }
