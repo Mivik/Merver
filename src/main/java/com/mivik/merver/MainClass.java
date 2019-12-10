d21566e7d71d5a696cbbbebf03f52859e78e4a8f
Mivik <mivik@qq.com>
2019-12-10 19:22:55 +0800

    add mainclass

--- src/main/java/com/mivik/merver/MainClass.java
+++ src/main/java/com/mivik/merver/MainClass.java
@@ -3,29 +3,39 @@ package com.mivik.merver;
 import java.io.File;
 
 public class MainClass {
-	public static void main(String[] args) {
-		File file = null;
-		int port = 1926;
-		for (int i = 0; i < args.length; i++) {
-			String arg = args[i];
-			if (arg.equals("--port") || arg.equals("-p")) {
-				if (i == args.length - 1) throw new IllegalArgumentException(args[i]);
-				port = Integer.parseInt(args[++i]);
-			} else {
-				file = new File(arg);
-				if (!file.exists()) {
-					System.err.println("File \"" + arg + "\" does not exist");
-					System.exit(1);
-					return;
-				}
-				if (!file.isDirectory()) {
-					System.err.println("Expected a direcotry");
-					System.exit(1);
-					return;
-				}
+	private static Merver merver = null;
+
+	private static void reportErr(String msg) {
+		System.err.println(msg);
+		System.exit(1);
 			}
+
+			private static void checkNull() {
+				if (merver == null) return;
+				reportErr("Only one merver can be specified");
+			}
+
+			public static void main(String[] args) {
+				int port = 1926;
+				for (int i = 0; i < args.length; i++) {
+					String arg = args[i];
+					if (arg.equals("--port") || arg.equals("-p")) {
+						if (i == args.length - 1) reportErr("Expected a number: " + arg);
+						port = Integer.parseInt(args[++i]);
+					} else if (arg.equals("--dir") || arg.equals("-d")) {
+						checkNull();
+						if (i == args.length - 1) reportErr("Expected a directory: " + arg);
+						arg = args[++i];
+						File file = new File(arg);
+						if (!file.exists()) reportErr("File \"" + arg + "\" does not exist");
+						if (!file.isDirectory()) reportErr("Expected a direcotry");
+				merver = new FileMerver(file);
+			} else if (arg.equals("--simple") || arg.equals("-s")) {
+				checkNull();
+				merver = new SimpleMerver();
+			} else reportErr("Unrecognized option: " + arg);
 		}
-		FileMerver merver = new FileMerver(file);
+		if (merver == null) reportErr("Merver not specified");
 		merver.getConfig().setVerbose(true);
 		merver.listen(port);
 	}
--- src/test/java/com/mivik/merver/TestClass.java
+++ src/test/java/com/mivik/merver/TestClass.java
@@ -1,18 +1,8 @@
 package com.mivik.merver;
 
-import com.mivik.merver.Merver;
-import com.mivik.merver.SimpleMerver;
-
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.util.Scanner;
-
 public class TestClass {
 	private static Merver merver;
 
 	public static void main(String[] args) throws Throwable {
-		merver = new SimpleMerver();
-		merver.getConfig().setVerbose(true);
-		merver.listen(1926);
 	}
 }
